404f0ce2c833c1781f8757e55f150ef1a3f81a21
Efrael <93404094+EfraelTino@users.noreply.github.com>
2022-10-14 17:05:14 -0500

    construccion de nuestro chequeo de compras

--- src/containers/Chequear.jsx
+++ src/containers/Chequear.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import '../styles/components/Chequear.css';
+
 const Chequear = () => {
     return (
-        <h1>Chequear</h1>
+        <div className='Checkout'>
+            <div className='Checkout-content'>
+                <h3>Lista de pedidos:</h3>
+                <div className='Checkout-item'>
+                    <div className='Checkout-element'>
+                        <h4>Nombre del producto</h4>
+                        <span>$50</span>
+                    </div>
+                    <button type='button'>Eliminar</button>
+                </div>
+            </div>
+            <div className='Checkout-sidebar'>
+                <h3>Precio total: $10</h3>
+                <button type='button'>Continuar pedido</button>
+            </div>
+        </div>
     );
 }
 
