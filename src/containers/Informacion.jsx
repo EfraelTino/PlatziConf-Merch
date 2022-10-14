66a1fdaf50c3c4a2b265f5fbb9718aa867409e83
Efrael <93404094+EfraelTino@users.noreply.github.com>
2022-10-14 17:16:13 -0500

    agregue la parte visuale de la compra

--- src/containers/Informacion.jsx
+++ src/containers/Informacion.jsx
@@ -3,7 +3,46 @@ import '../styles/components/Informacion.css'
 
 const Informacion = () => {
     return (
-        <h1>Informacion</h1>
+        <div className='Information'>
+            <div className='Information-content'>
+                <div className='Information-head'>
+                    <h2>Informacion de contacto:</h2>
+                </div>
+                <div className='Information-form'>
+                    <form action=''>
+                        <input type='text' placeholder='Nombre completo' name='name'/>
+                        <input type='email' placeholder='Correo electronico' name='email'/>
+                        <input type='text' placeholder='Direccion' name='address'/>
+                        <input type='text' placeholder='Apto' name='apto'/>
+                        <input type='text' placeholder='Ciudad' name='city'/>
+                        <input type='text' placeholder='Pais' name='country'/>
+                        <input type='text' placeholder='Estado' name='state'/>
+                        <input type='text' placeholder='Código postal' name='cp'/>
+                        <input type='text' placeholder='Teléfono' name='phone'/>
+                    </form>
+                </div>
+                {/* regresar o ir a comprar */}
+                <div className='Information-buttons'>
+                    <div className='Information-back'>
+                        Regresar
+                    </div>
+                    <div className='Information-next'>
+                        Pagar
+                    </div>
+                </div>
+            </div>
+            <div className='Information-sidebar'>
+                <h3>Pedido:</h3>
+                <div className='Information-item'>
+                    <div className='Information-element'>
+                        <h4>
+                            Nombre del item
+                        </h4>
+                        <span> $10 </span>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
